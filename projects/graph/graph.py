f74626e90bcd833c8089ffc7a246a48ee3a37349
EpiceneDev <48662221+EpiceneDev@users.noreply.github.com>
2020-07-20 12:34:58 -0700

    added vertex code

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -8,18 +8,19 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {
-    '0': {'1', '3'},
-    '1': {'0'},
-    '2': set(),
-    '3': {'0'}
-}
+        '0': {'1', '3'},
+        '1': {'0'},
+        '2': set(),
+        '3': {'0'}
+        }   
 
 
     def add_vertex(self, vertex_id):
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
+
 
     def add_edge(self, v1, v2):
         """
